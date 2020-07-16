39a1bdc50c344ac1d75018813cb20fc9e5ebf08d
Alec Keen <aleckeen@tutanota.com>
2020-07-16 14:46:23 -0500

    Added tests

--- src/lib.rs
+++ src/lib.rs
@@ -16,3 +16,27 @@ pub fn home_dir() -> Option<PathBuf>
             }
         })
 }
+
+#[cfg(test)]
+mod tests
+{
+    use std::path::PathBuf;
+
+    use super::{home_dir, xdg};
+
+    fn into_str(p: Option<PathBuf>) -> String
+    {
+        p.and_then(|p| Some(format!("{}", p.display()))).unwrap_or(String::from("None"))
+    }
+
+    #[test]
+    fn print_all()
+    {
+        println!();
+        println!("home_dir() ---------> {}", into_str(home_dir()));
+        println!("xdg::config_home() -> {}", into_str(xdg::config_home()));
+        println!("xdg::cache_home() --> {}", into_str(xdg::cache_home()));
+        println!("xdg::data_home() ---> {}", into_str(xdg::data_home()));
+        println!();
+    }
+}
