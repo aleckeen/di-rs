5effe00ccafd736495e4defe61310d530f0a273e
Alec Keen <aleckeen@tutanota.com>
2020-07-16 14:15:30 -0500

    Create function get_xdg_path

--- src/xdg.rs
+++ src/xdg.rs
@@ -1,21 +1,28 @@
 use std::env;
-use std::path::PathBuf;
+use std::path::{PathBuf, Path};
 
 use crate::home_dir;
+use std::ffi::OsStr;
 
 const CONFIG_HOME: &'static str = "XDG_CONFIG_HOME";
+const CACHE_HOME: &'static str = "XDG_CACHE_HOME";
 
-pub fn config_home() -> Option<PathBuf>
+fn get_xdg_path<K: AsRef<OsStr>, P: AsRef<Path>>(key: K, suffix: P) -> Option<PathBuf>
 {
-    env::var_os(CONFIG_HOME)
+    env::var_os(key)
         .and_then(|p| {
             if p.is_empty() {
                 home_dir()
                     .and_then(|h| {
-                        Some(h.join(".config"))
+                        Some(h.join(suffix))
                     })
             } else {
                 Some(PathBuf::from(p))
             }
         })
+}
+
+pub fn config_home() -> Option<PathBuf>
+{
+    get_xdg_path(CONFIG_HOME, ".config")
 }
